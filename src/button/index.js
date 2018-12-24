acfd4d6b32f6979ded925ee40e799965a85fa1e8
yangglive <yangglive@icloud.com>
2018-12-24 09:28:41 +0800

    add main

--- src/button/index.js
+++ src/button/index.js
@@ -4,17 +4,32 @@
 
 
 import React, {Component} from "react";
+import PropTypes from "prop-types";
 
 export default class Button extends Component {
+
+    static propTypes = {
+        type: PropTypes.string,
+        size: PropTypes.oneOf(['large', 'default', 'small']),
+        htmlType: PropTypes.oneOf(['submit', 'button', 'reset']),
+    };
+
     static defaultProps = {
         prefixClass: "lemon-button",
-        type: "text",
+        type: "default",
     };
+
     render() {
-        const {style, className, prefixClass, ...rest} = this.props;
+        const {style, type, size, className, prefixClass, ...rest} = this.props;
         let thisClassName = prefixClass;
         if (className) {
             thisClassName += " " + className;
+        }
+        if (size) {
+            thisClassName += " " + prefixClass + "-" + size;
+        }
+        if (type) {
+            thisClassName += " " + prefixClass + "-" + type;
         }
         return <button {...rest} className={thisClassName} style={style}>{this.props.children}</button>
     }
